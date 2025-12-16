bb756cf529498f4cfc972197185fcb989afb6171
Angela McEgo <eve.loikkanen@gmail.com>
2025-12-16 03:01:34 +0200

    more 8087

--- tools/8087mc/main.cpp
+++ tools/8087mc/main.cpp
@@ -80,8 +80,8 @@ std::string make_cond_string(int cond_int)
     else if (cond_code == 0x23) cond += " stack overflow";
     else if (cond_code == 0x24) cond += " tmpB zero";
     else if (cond_code == 0x26) cond += " tmpB bad?";
-    else if (cond_code == 0x27) cond += " st(i) doesn't exist (A)";
-    else if (cond_code == 0x2f) cond += " st(i) doesn't exist (B)";
+    else if (cond_code == 0x27) cond += " st(i) doesn't exist (A)?";
+    else if (cond_code == 0x2f) cond += " st(i) doesn't exist (B)?";
     else if (cond_code == 0x38) cond += " accumulator!=0";
     else if (cond_code == 0x3e) cond += " unconditional";
 
@@ -101,6 +101,17 @@ void decodeMicroOp(uint16_t op, std::string comment, int lineNum)
     std::cout << std::hex << std::setw(4) << std::setfill('0') << op << std::dec << "\t";
 
     // ABCD EFGH IJKL MNOP
+        const char*const regs[] =
+        {
+            "[0x0]", "[0x1]", "[0x2]st(0)", "[0x3]st(i)",
+            "[0x4]", "[0x5 sum?]", "[0x6]sum input", "[0x7]",
+            "[0x8]expA", "[0x9]", "[0xa]mantA", "[0xb]tmpA",
+            "[0xc]expB", "[0xd]", "[0xe]mantB", "[0xf]tmpB",
+            "[0x10]", "[0x11]", "[0x12]", "[0x13]",
+            "[0x14]", "[0x15]", "[0x16]", "[0x17]",
+            "[0x18]", "[0x19]", "[0x1a]tmpA.X", "[0x1b]tmpA.Y",
+            "[0x1c]", "[0x1d]", "[0x1e]", "[0x1f]NaN?",
+        };
 
 
     if (((op & 0xFFF0) == 0xF000) && ((op&0xF)<6*2)) //set exception
@@ -135,24 +146,21 @@ void decodeMicroOp(uint16_t op, std::string comment, int lineNum)
             std::cout << "+jmp->#" << std::setw(4) << lineNum+1+offset << " " << cond;
         }
     }
-    else if ((op&0xF07F) == 0x0078)
+    /*else if ((op&0xF07F) == 0x0078)
     {
         std::cout << "?load constant [0x" << std::hex << ((op>>7)&0x01F) << std::dec << "]?";
-    }
-    else if ((op&0xE0C1) == 0)
+    }*/
+    else if ((op&0xE0C0) == 0)
     {
-        const char*const regs[] =
-        {
-            "[0x0]", "[0x1]", "[0x2]st(0)", "[0x3]st(i)",
-            "[0x4]", "[0x5]?constant?", "[0x6]zero", "[0x7]",
-            "[0x8]", "[0x9]", "[0xa]", "[0xb]tmpA",
-            "[0xc]", "[0xd]", "[0xe]", "[0xf]tmpB",
-            "[0x10]", "[0x11]", "[0x12]", "[0x13]",
-            "[0x14]", "[0x15]", "[0x16]", "[0x17]",
-            "[0x18]", "[0x19]", "[0x1a] st0 something?", "[0x1b] sti something?",
-            "[0x1c]", "[0x1d]", "[0x1e]", "[0x1f]NaN?",
-        };
-        std::cout << regs[(op>>8)&0x1F] << " -> " << regs[(op>>1)&0x1F];
+        std::cout << regs[(op>>8)&0x1F] << " -> " << regs[(op>>1)&0x1F] << ((op&1)?" with P":"");
+    }
+    else if ((op&0xE0C0) == 0x00C0)
+    {
+        std::cout << "IJ mov /0x" << std::hex << ((op>>8)&0x1F) << std::dec << "/ -> " << regs[(op>>1)&0x1F] << ((op&1)?" with P":"");
+    }
+    else if ((op&0xE0C0) == 0x0040)
+    {
+        std::cout << "-J mov /0x" << std::hex << ((op>>8)&0x1F) << std::dec << "/ -> " << regs[(op>>1)&0x1F] << ((op&1)?" with P":"");
     }
     else if ((op&0xF000) == 0xA000) //A.C.
     {
@@ -190,7 +198,20 @@ void decodeMicroOp(uint16_t op, std::string comment, int lineNum)
     else if (op == 0xe802) std::cout << "?decrement something A?";
 
     else if (op == 0x7c00) std::cout << "?increment something B?";
-    else if (op == 0x7c02) std::cout << "?decrement something B ?";
+    else if (op == 0x7c02) std::cout << "?decrement something B?";
+
+    else if ((op&0xfc78) == 0x6048) // set C0=G, C3=H
+    {
+        std::cout << "set ";
+        std::cout << "C0=" << ((op&0x0200)?1:0) << ", ";
+        std::cout << "C2=" << ((op&0x0080)?1:0) << ", ";
+        std::cout << "C3=" << ((op&0x0100)?1:0) << ", ";
+        std::cout << "NO=" << ((op>>1)&0x03);
+    }
+    else if ((op&0xff7f) == 0x604e) // set C2=1
+    {
+        std::cout << "set C2=" << ((op&0x080)?1:0);
+    }
 
 
     else if (op == 0x600a)
@@ -213,17 +234,22 @@ void decodeMicroOp(uint16_t op, std::string comment, int lineNum)
     else if (op == 0x6380)
         std::cout << "?sgn take stored sign, flip, and store to tmpA?"; //.BC...GHI.......
 
-    else if (op == 0xec00) std::cout << "?swap tmpA and tmpB?"; // ABC.EF..........
+    else if (op == 0xec00) std::cout << "?negate something?"; // ABC.EF..........
 
 
-    else if (op == 0x5006)
+    else if ((op&0xE001)==0x4000)
+    {
+        std::cout << "Bop! param=" << regs[(op>>8)&0x1F] << " operation=" << ((op>>1)&0x7F);
+    }
+
+    /*else if (op == 0x5006)
     {
         std::cout << "?increment tmpA exponent?";
     }
     else if (op == 0x5008)
     {
         std::cout << "?decrement tmpA exponent?";
-    }
+    }*/
     else
     {
         std::cout << "?";
